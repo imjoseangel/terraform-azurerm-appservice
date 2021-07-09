6a56b0467def79aab4a31a3e06b0d06e7b169e0e
imjoseangel <josea.munoz@gmail.com>
2021-07-09 13:56:50 +0200

    remove deprecated

--- main.tf
+++ main.tf
@@ -41,10 +41,13 @@ resource "azurerm_resource_group" "rg" {
 #---------------------------------------------------------
 
 resource "azuread_application" "main" {
-  display_name               = format("%s-%s", var.prefix, lower(replace(var.name, "/[[:^alnum:]]/", "")))
-  identifier_uris            = [format("api://%s", lower(replace(var.name, "/[[:^alnum:]]/", "")))]
-  sign_in_audience           = "AzureADMyOrg"
-  oauth2_allow_implicit_flow = true
+  display_name     = format("%s-%s", var.prefix, lower(replace(var.name, "/[[:^alnum:]]/", "")))
+  identifier_uris  = [format("api://%s", lower(replace(var.name, "/[[:^alnum:]]/", "")))]
+  sign_in_audience = "AzureADMyOrg"
+
+  implicit_grant {
+    access_token_issuance_enabled = true
+  }
 }
 
 #---------------------------------------------------------
