56802c052cd9e307aeba452369b1d6d214c65f37
imjoseangel <josea.munoz@gmail.com>
2021-07-21 10:45:53 +0200

    add output

---
+++ outputs.tf
@@ -0,0 +1,24 @@
+output "app_service_plan_id" {
+  description = "Id of the App Service Plan"
+  value       = var.app_service_plan_id
+}
+
+output "app_service_id" {
+  description = "Id of the App Service"
+  value       = azurerm_app_service.main.id
+}
+
+output "app_service_name" {
+  description = "Name of the App Service"
+  value       = azurerm_app_service.main.name
+}
+
+output "app_service_outbound_ip_addresses" {
+  description = "Outbound IP adresses of the App Service"
+  value       = split(",", azurerm_app_service.main.outbound_ip_addresses)
+}
+
+output "app_service_possible_outbound_ip_addresses" {
+  description = "Possible outbound IP adresses of the App Service"
+  value       = split(",", azurerm_app_service.main.possible_outbound_ip_addresses)
+}
