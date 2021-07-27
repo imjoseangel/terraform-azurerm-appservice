2865aacba3c0a8a0f0a32b1bb7ffd400e40d2ca0
imjoseangel <josea.munoz@gmail.com>
2021-07-27 11:35:09 +0200

    add iconfig

--- main.tf
+++ main.tf
@@ -21,7 +21,6 @@ locals {
   app_insights = try(data.azurerm_application_insights.main.0, {})
 
   default_app_settings = var.application_insights_enabled ? {
-    APPLICATION_INSIGHTS_IKEY             = try(local.app_insights.instrumentation_key, "")
     APPINSIGHTS_INSTRUMENTATIONKEY        = try(local.app_insights.instrumentation_key, "")
     APPLICATIONINSIGHTS_CONNECTION_STRING = try(local.app_insights.connection_string, "")
   } : {}
