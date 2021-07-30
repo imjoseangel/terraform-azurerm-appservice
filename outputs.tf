eada6d5a65ab0552b9e87bf9fb44183860f5ea8a
imjoseangel <josea.munoz@gmail.com>
2021-07-30 19:54:29 +0200

    add sp ouput

--- outputs.tf
+++ outputs.tf
@@ -22,3 +22,8 @@ output "app_service_possible_outbound_ip_addresses" {
   description = "Possible outbound IP adresses of the App Service"
   value       = split(",", azurerm_app_service.main.possible_outbound_ip_addresses)
 }
+
+output "azuread_service_principal" {
+  description = "Azure Service Principal Object ID"
+  value       = azuread_service_principal.main.object_id
+}
