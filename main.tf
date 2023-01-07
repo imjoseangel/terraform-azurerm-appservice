42cb16c0db796df0b1b0ed78650f596970442123
imjoseangel <josea.munoz@gmail.com>
2023-01-07 22:46:35 +0100

    add linting

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@
 # Local Declarations
 #-------------------------------
 locals {
-  resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp.*.name, azurerm_resource_group.rg.*.name, [""]), 0)
-  location            = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, azurerm_resource_group.rg.*.location, [""]), 0)
+  resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp[*].name, azurerm_resource_group.rg[*].name, [""]), 0)
+  location            = element(coalescelist(data.azurerm_resource_group.rgrp[*].location, azurerm_resource_group.rg[*].location, [""]), 0)
 
   default_site_config = {
     default_documents = [
