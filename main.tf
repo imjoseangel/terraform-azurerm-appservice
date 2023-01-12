5f5ab80e28d1e4a8a1255c69e6b26cef05b25f1d
imjoseangel <josea.munoz@gmail.com>
2023-01-12 22:20:59 +0100

    add linux slot

--- main.tf
+++ main.tf
@@ -162,99 +162,27 @@ resource "azurerm_linux_web_app" "main" {
   }
 }
 
-resource "azurerm_app_service" "main" {
-  name                = lower(var.name)
-  location            = local.location
-  resource_group_name = local.resource_group_name
-  app_service_plan_id = var.app_service_plan_id
+resource "azurerm_linux_web_app_slot" "staging" {
+  name           = var.slot_name
+  app_service_id = azurerm_linux_web_app.main.id
+  app_settings   = merge(local.default_app_settings, var.app_settings)
 
   dynamic "site_config" {
     for_each = [merge(local.default_site_config, var.site_config)]
 
     content {
-      always_on                 = lookup(site_config.value, "always_on", null)
-      app_command_line          = lookup(site_config.value, "app_command_line", null)
-      default_documents         = lookup(site_config.value, "default_documents", null)
-      dotnet_framework_version  = lookup(site_config.value, "dotnet_framework_version", null)
-      ftps_state                = lookup(site_config.value, "ftps_state", null)
-      health_check_path         = lookup(site_config.value, "health_check_path", null)
-      number_of_workers         = lookup(site_config.value, "number_of_workers", null)
-      http2_enabled             = lookup(site_config.value, "http2_enabled", null)
-      linux_fx_version          = lookup(site_config.value, "linux_fx_version", null)
-      windows_fx_version        = lookup(site_config.value, "windows_fx_version", null)
-      managed_pipeline_mode     = lookup(site_config.value, "managed_pipeline_mode", null)
-      min_tls_version           = lookup(site_config.value, "min_tls_version", null)
-      python_version            = lookup(site_config.value, "python_version", null)
-      remote_debugging_enabled  = lookup(site_config.value, "remote_debugging_enabled", null)
-      remote_debugging_version  = lookup(site_config.value, "remote_debugging_version", null)
-      scm_type                  = lookup(site_config.value, "scm_type", null)
-      use_32_bit_worker_process = lookup(site_config.value, "use_32_bit_worker_process", null)
-      websockets_enabled        = lookup(site_config.value, "websockets_enabled", null)
-    }
-  }
-
-  dynamic "auth_settings" {
-    for_each = var.auth_settings_enabled ? ["auth_settings_enabled"] : []
-    content {
-      enabled                        = var.auth_settings_enabled
-      issuer                         = format("https://sts.windows.net/%s/v2.0", data.azurerm_client_config.current.tenant_id)
-      token_store_enabled            = false
-      unauthenticated_client_action  = "RedirectToLoginPage"
-      default_provider               = "AzureActiveDirectory"
-      allowed_external_redirect_urls = []
-
-      active_directory {
-        client_id         = azuread_application.main[0].application_id
-        allowed_audiences = [format("api://%s", azuread_application.main[0].application_id)]
-      }
-    }
-  }
-
-  https_only = var.https_only
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = merge({ "ResourceName" = lower(var.name) }, var.tags, )
-
-  lifecycle {
-    ignore_changes = [
-      tags, identity[0].identity_ids, app_settings
-    ]
-  }
-}
-
-resource "azurerm_app_service_slot" "staging" {
-  name                = var.slot_name
-  app_service_name    = azurerm_app_service.main.name
-  location            = local.location
-  resource_group_name = local.resource_group_name
-  app_service_plan_id = var.app_service_plan_id
-  app_settings        = merge(local.default_app_settings, var.app_settings)
-
-  dynamic "site_config" {
-    for_each = [merge(local.default_site_config, var.site_config)]
-
-    content {
-      always_on                 = lookup(site_config.value, "always_on", null)
-      app_command_line          = lookup(site_config.value, "app_command_line", null)
-      default_documents         = lookup(site_config.value, "default_documents", null)
-      dotnet_framework_version  = lookup(site_config.value, "dotnet_framework_version", null)
-      ftps_state                = lookup(site_config.value, "ftps_state", null)
-      health_check_path         = lookup(site_config.value, "health_check_path", null)
-      number_of_workers         = lookup(site_config.value, "number_of_workers", null)
-      http2_enabled             = lookup(site_config.value, "http2_enabled", null)
-      linux_fx_version          = lookup(site_config.value, "linux_fx_version", null)
-      windows_fx_version        = lookup(site_config.value, "windows_fx_version", null)
-      managed_pipeline_mode     = lookup(site_config.value, "managed_pipeline_mode", null)
-      min_tls_version           = lookup(site_config.value, "min_tls_version", null)
-      python_version            = lookup(site_config.value, "python_version", null)
-      remote_debugging_enabled  = lookup(site_config.value, "remote_debugging_enabled", null)
-      remote_debugging_version  = lookup(site_config.value, "remote_debugging_version", null)
-      scm_type                  = lookup(site_config.value, "scm_type", null)
-      use_32_bit_worker_process = lookup(site_config.value, "use_32_bit_worker_process", null)
-      websockets_enabled        = lookup(site_config.value, "websockets_enabled", null)
+      always_on                = lookup(site_config.value, "always_on", null)
+      app_command_line         = lookup(site_config.value, "app_command_line", null)
+      default_documents        = lookup(site_config.value, "default_documents", null)
+      ftps_state               = lookup(site_config.value, "ftps_state", null)
+      health_check_path        = lookup(site_config.value, "health_check_path", null)
+      http2_enabled            = lookup(site_config.value, "http2_enabled", null)
+      linux_fx_version         = lookup(site_config.value, "linux_fx_version", null)
+      managed_pipeline_mode    = lookup(site_config.value, "managed_pipeline_mode", null)
+      remote_debugging_enabled = lookup(site_config.value, "remote_debugging_enabled", null)
+      remote_debugging_version = lookup(site_config.value, "remote_debugging_version", null)
+      scm_type                 = lookup(site_config.value, "scm_type", null)
+      websockets_enabled       = lookup(site_config.value, "websockets_enabled", null)
     }
   }
 
@@ -292,7 +220,7 @@ resource "azurerm_app_service_slot" "staging" {
 
 resource "azurerm_app_service_virtual_network_swift_connection" "main" {
   count          = var.vnet_integration ? 1 : 0
-  app_service_id = azurerm_app_service.main.id
+  app_service_id = azurerm_linux_web_app.main.id
   subnet_id      = var.vnet_subnet_id
   lifecycle {
     ignore_changes = [
@@ -303,8 +231,8 @@ resource "azurerm_app_service_virtual_network_swift_connection" "main" {
 
 resource "azurerm_app_service_slot_virtual_network_swift_connection" "slot" {
   count          = var.slot_vnet_integration ? 1 : 0
-  slot_name      = azurerm_app_service_slot.staging.name
-  app_service_id = azurerm_app_service.main.id
+  slot_name      = azurerm_linux_web_app_slot.staging.name
+  app_service_id = azurerm_linux_web_app.main.id
   subnet_id      = var.vnet_subnet_id
   lifecycle {
     ignore_changes = [
@@ -322,7 +250,7 @@ resource "azurerm_private_endpoint" "main" {
 
   private_service_connection {
     name                           = "privateendpointconnection"
-    private_connection_resource_id = azurerm_app_service.main.id
+    private_connection_resource_id = azurerm_linux_web_app.main.id
     subresource_names              = ["sites"]
     is_manual_connection           = false
   }
--- outputs.tf
+++ outputs.tf
@@ -5,22 +5,22 @@ output "app_service_plan_id" {
 
 output "app_service_id" {
   description = "Id of the App Service"
-  value       = azurerm_app_service.main.id
+  value       = azurerm_linux_web_app.main.id
 }
 
 output "app_service_name" {
   description = "Name of the App Service"
-  value       = azurerm_app_service.main.name
+  value       = azurerm_linux_web_app.main.name
 }
 
 output "app_service_outbound_ip_addresses" {
   description = "Outbound IP adresses of the App Service"
-  value       = split(",", azurerm_app_service.main.outbound_ip_addresses)
+  value       = split(",", azurerm_linux_web_app.main.outbound_ip_addresses)
 }
 
 output "app_service_possible_outbound_ip_addresses" {
   description = "Possible outbound IP adresses of the App Service"
-  value       = split(",", azurerm_app_service.main.possible_outbound_ip_addresses)
+  value       = split(",", azurerm_linux_web_app.main.possible_outbound_ip_addresses)
 }
 
 output "azuread_service_principal" {
